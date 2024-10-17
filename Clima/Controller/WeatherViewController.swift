bbf568466b30cba3298bdeb4f6b5de3326cbafd4
Muhammetkocaman <kocamamm@gmail.com>
2024-10-17 15:04:54 +0300

    Learned what protocols, delegate design pattern, http request

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,24 +1,62 @@
-//
-//  ViewController.swift
-//  Clima
-//
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController,UITextFieldDelegate {
+    // outlets
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
+    
+    var weatherManager =  WeatherManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
+        
+        // close keyboard anywhere touching
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchTextField.endEditing(true)
+        
+        print(searchTextField.text!)
+        
+        
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Please enter a city"
+            return false
+        }
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // use the texField.text to the get weather
+        if let city = textField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
+        
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -1,9 +1,37 @@
-//
-//  File.swift
-//  Clima
-//
-//  Created by Muhammet Emin Kocaman on 17.10.2024.
-//  Copyright © 2024 App Brewery. All rights reserved.
-//
+
 
 import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=7b313b5d9e8ec632b31dd871a50513d3&units=metric"
+    func fetchWeather(cityName: String)  {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    func performRequest(urlString : String)  {
+        //1. Create Url
+        if let url = URL(string: urlString) {
+            //2. Create URLsession
+            let session = URLSession(configuration: .default)
+            //3. Give a session task
+            let task = session.dataTask(with: url,completionHandler: handle(data:response:error:) )
+            //4. Start the task
+            task.resume()
+        }
+        
+        
+        
+    }
+    func handle(data : Data?,response : URLResponse?, error : Error?) {
+        if error != nil {
+            print(error ?? "Unknown error")
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+    
+    
+}
